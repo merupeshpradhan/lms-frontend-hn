de781809953cd393c2b07e8b1b11ffcda94580d9
Rupesh pradhan <merupeshpradhan@gmail.com>
2025-01-21 22:46:43 +0530

    Added sign in functionality

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,18 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import AboutUs from "./Pages/AboutUs";
 import NotFound from "./Pages/NotFound";
+import Signup from "./Pages/Signup";
+import Login from "./Pages/Login";
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/about" element={<AboutUs />}></Route>
+
+        <Route path="/signup" element={<Signup />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+
         <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </>
